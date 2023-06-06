c563b1585ac5717d52ae5db44cd6784945d24ca6
VARES <98279770+Im-Vares@users.noreply.github.com>
2023-06-06 09:32:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,61 @@
-const Discord = require('discord.js') // подключение библиотеки               
-const client = new Discord.Client() // создание клиента
+const Discord = require('discord.js')
+const client = new Discord.Client({intents: 32767}) 
+const keepAlive = require("./server")  
+const guildInvites = new Map();
 
-client.on('ready', () =>{ // ивент, когда бот запускается https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-ready
-    client.generateInvite("ADMINISTRATOR").then(invite => console.log(`Ссылка на добавление ${invite}`))
-    console.log(`Привет! ${client.user.tag} запустился!`) // информация в консоль про успешный запуск
+client.on('ready', () =>{ 
+    console.log(`Привет! ${client.user.tag} запустился!`)
+      
 })
 
-client.on('message', message =>{ // ивент, когда приходит любое сообщение в чат https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-message
-    if (message.author.bot) return; // если автор сообщения - бот, ничего не происходит 
-    if (message.content == '!профиль') { // если пользователь написал "!профиль" 
-    let embed = new Discord.MessageEmbed() // создание ембед сообщения
-    .setTitle(message.author.username) // в тайтле имя автора 
+
+const { joinVoiceChannel } = require('@discordjs/voice');
+client.on('messageCreate', message => {
+    if(message.content === '!join') {
+        joinVoiceChannel({
+            channelId: message.member.voice.channel.id,
+            guildId: message.guild.id,
+            adapterCreator: message.guild.voiceAdapterCreator
+        })
+    }
+})
+
+
+
+
+client.on("voiceStateUpdate",(oldState,newState)=>{
+    var categoryid = "category.id";
+    var channelid = "channel.id";
+
+    if(newState.channel?.id == channelid){
+        newState.guild.channels.create(`Приватка ${newState.member.user.username} `,{
+            type:"GUILD_VOICE",
+            parent:categoryid,
+            permissionOverwrites:[{
+                id:newState.member.id,
+                allow: ["MANAGE_CHANNELS"]
+            },{
+                id:newState.guild.id,
+                deny:["MANAGE_CHANNELS"]
+            }] 
+        }).then(channel=>{
+            newState.setChannel(channel)
+            channel.createInvite({
+                maxAge:0,
+                maxUses:0,
+           
+            }).then(inv=>newState.member.send(inv.toString()))
+        })
+    } 
+    if(oldState.channel?.id != channelid && oldState.channel?.parent?.id == categoryid && !oldState.channel?.members.size) oldState.channel.delete();
+})
+
+
+client.on('messageCreate', message =>{ 
+    if (message.author.bot) return; 
+    if (message.content == '!профиль') { 
     let status = ''
-    switch (message.author.presence.status) { // проверка статусов 
+    switch (message.member.presence.status) { 
         case 'online':
             status = 'онлайн'; break; 
             case 'idle':
@@ -22,66 +65,60 @@ client.on('message', message =>{ // ивент, когда приходит лю
                     case 'dnd':
                         status = ':red_circle:не беспокоить'; break;
     }
-    embed.setDescription(`**Ваш дискорд айди: ${message.author.id}
+      let embed = new Discord.MessageEmbed() 
+    .setTitle(`${message.author.username}`) //вот так
+    .setDescription(`**Ваш дискорд айди: ${message.author.id}
     Ваш статус: ${status}
     Дата создания аккаунта: ${message.author.createdAt.toLocaleDateString()}
     Дата входа на сервер: ${message.member.joinedAt.toLocaleDateString()}
-    **`) // описание ембеда
-    .setColor('RANDOM') // рандомный цвет ембеда
-    .setThumbnail(message.author.avatarURL()) // вставляем в ембед аватарку пользователя
-    message.channel.send(embed) // отправляем сообщение в канал где была написана команда   
+    **`) 
+    .setColor('RANDOM') 
+    .setThumbnail(message.author.avatarURL()) 
+    message.channel.send({embeds: [embed]})
     }
 })
 
-client.on('messageDelete', message =>{ // ивент, когда удаляется любое сообщение с сервера https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildMemberAdd
-    let embed = new Discord.MessageEmbed()
+client.on('messageDelete', message =>{ 
+   if (message.author.bot) return;
+    let embed12 = new Discord.MessageEmbed()
     .setTitle('Было удалено сообщение!')
     .setColor('RANDOM')
-    .addField(`Удалённое сообщение:`, message.content, true)
+    .addField(`Удалённое сообщение:`, `${message.content|| `отсутствует`}`, true)
     .addField("Автор:",`${message.author.tag} (${message.author})`,true)
     .addField("Канал:", `${message.channel}`, false)
     .setFooter(' - ',`${message.author.avatarURL()}`)
     .setTimestamp(message.createdAt);
-  client.channels.cache.get("АЙДИ КАНАЛА С ЛОГАМИ").send(embed); // айди вашего канала с логами
+  client.channels.cache.get("channel.token").send({embeds: [embed12]})
 })
 
-client.on('guildMemberAdd', member =>{ // ивент, когда пользователь присоединяется к серверу https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildMemberAdd
-    let embed = new Discord.MessageEmbed()
+
+client.on('guildMemberAdd', member =>{ 
+    let embed11 = new Discord.MessageEmbed()
     .setThumbnail(member.user.avatarURL())
     .setTitle(`Привет, ${member.user.username}!`)
-    .setDescription(`**Ты попал на мой сервер!
-    Ты наш \`${client.guilds.get("АЙДИ СЕРВЕРА").memberCount}\` участник! **`) // айди вашего сервера               !!!!!!!!!!
+    .setDescription(`Ты попал на сервер `)
     .setFooter('Будь всегда на позитиве :3', 'https://cdn.discordapp.com/emojis/590614597610766336.gif?v=1')
-    // .addField(`Участвуй в розыгрышах!`, `<#706487236220289054>`, true) // Добавляйте свои каналы по желанию
-    // .addField(`Общайся в чате!`, `<#702364684199788625>`, true)
-
     .setColor('RANDOM')
-    member.send(embed); // отправка сообщения в лс 
+    member.send({embeds: [embed11]}); 
 
     let embed2 = new Discord.MessageEmbed()
     .setThumbnail(member.user.avatarURL())
     .setTitle(`Пользователь вошел на сервер`)
-    .addField('Пользователь:', member.user)
+     .addField('Пользователь:', `${member.user}`)
     .setColor('RANDOM')
-    member.send(embed);
-    client.channels.cache.get('АЙДИ КАНАЛА С ЛОГАМИ').send(embed2) // айди вашего канала с логами
+    client.channels.cache.get('channel.token').send({embeds: [embed2]})
 })
 
 client.on('guildMemberRemove', member => { 
     let embed = new Discord.MessageEmbed()
     .setThumbnail(member.user.avatarURL())
     .setTitle(`Пользователь покинул сервер`)
-    .addField('Пользователь:', member.user)
+     .addField('Пользователь:', `${member.user}`)
     .setColor('RANDOM')
-    member.send(embed);
-    client.channels.cache.get('АЙДИ КАНАЛА С ЛОГАМИ').send(embed) // айди вашего канала с логами
+    client.channels.cache.get('channel.token').send({embeds: [embed]})
   })
 
-async function change() {
-    let members = client.guilds.cache.get("АЙДИ ВОЙСА").memberCount // сколько людей на сервере + указать айди своего сервера
-    client.channels.cache.get("АЙДИ СЕРВЕРА").setName(`На сервере: ${members}`); // свой айди войса
-}
 
-var interval = setInterval(function () { change(); }, 20000  ); // время обновления в миллисекундах
+keepAlive()
 
-client.login(process.env.BOT_TOKEN) // токен вашего бота
+client.login(process.env.BOT_TOKKEN) 
